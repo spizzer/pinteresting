7ec6d50b44ed95303d673302da3046c3da549138
Spencer <spencer_smalley@hotmail.com>
2014-03-08 01:26:42 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinfestering::Application.config.secret_key_base = 'c6920685cc58b49f8a366d86d9bddd797c7f9f20cf6e35223842ae3bc126ee562abde3dd0dcb1993d8b5bfaffac989879a594effd7fea3c351a8bd68dd269968'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pinfestering::Application.config.session_store :cookie_store, key: '_pinfestering_session'
